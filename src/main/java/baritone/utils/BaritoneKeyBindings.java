c45086233462aa17ad8b1875085960b865fa1770
Tr0ngX <Tr0ngX@users.noreply.github.com>
2026-09-10 15:06:23 +0700

    refactor(keybind): restore F4 as single GUI key, remove extra key mappings

--- src/main/java/baritone/utils/BaritoneKeyBindings.java
+++ src/main/java/baritone/utils/BaritoneKeyBindings.java
@@ -33,28 +33,7 @@ public final class BaritoneKeyBindings {
     public static final KeyMapping KEY_AUTOMINE_GUI = new KeyMapping(
             "key.baritone.automine_gui",
             InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_M,
-            CATEGORY
-    );
-
-    public static final KeyMapping KEY_NEVER_KICK = new KeyMapping(
-            "key.baritone.never_kick",
-            InputConstants.Type.KEYSYM,
-            InputConstants.UNKNOWN.getValue(),
-            CATEGORY
-    );
-
-    public static final KeyMapping KEY_AUTO_LOGOUT = new KeyMapping(
-            "key.baritone.auto_logout",
-            InputConstants.Type.KEYSYM,
-            InputConstants.UNKNOWN.getValue(),
-            CATEGORY
-    );
-
-    public static final KeyMapping KEY_PANIC_LOGOUT = new KeyMapping(
-            "key.baritone.panic_logout",
-            InputConstants.Type.KEYSYM,
-            InputConstants.UNKNOWN.getValue(),
+            GLFW.GLFW_KEY_F4,
             CATEGORY
     );
 
@@ -74,9 +53,6 @@ public final class BaritoneKeyBindings {
 
     public static final KeyMapping[] ALL_KEYS = new KeyMapping[]{
             KEY_AUTOMINE_GUI,
-            KEY_NEVER_KICK,
-            KEY_AUTO_LOGOUT,
-            KEY_PANIC_LOGOUT,
             KEY_CANCEL,
             KEY_PAUSE
     };
